01c3d011d049595e5aee1a86c5f92093062aea19
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2022-11-11 11:57:41 +0100

    Remove unnecessary comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                ------------------------------------
-                Programowanie Efektywnych Algorytmów
-                Zadanie projektowe nr 1 ------------
-                Igor Stalmach, 259116 --------------
-                ------------------------------------
-                """);
 
         GUI gui = new GUI();
 
